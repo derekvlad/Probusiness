13e55ecd25a0eeaf1c46704d20624cc31da60a4f
derekvlad <bravis96166@gmail.com>
2022-09-18 17:28:13 +0300

    add adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -3,6 +3,16 @@ $(function () {
         dots: true,
         prevArrow: '<button class="slick-prev slick-arrow" aria-label="Previous" type="button" style=""><img src="images/icon/arrow-left.svg" alt=""></button>',
         nextArrow: '<button class="slick-next slick-arrow" aria-label="Next" type="button" style=""><img src="images/icon/arrow-right.svg" alt=""></button>',
+        responsive: [
+            {
+                breakpoint: 950,
+                settings: {
+                    arrows: false,
+                    autoplay: true,
+                    autoplaySpeed: 3500,
+                }
+            },
+        ]
     });
     $('.news__slider').slick({
         prevArrow: '<button class="slick-prev slick-arrow" aria-label="Previous" type="button" style=""><img src="images/icon/arrow-left.svg" alt=""></button>',
@@ -10,5 +20,29 @@ $(function () {
         slidesToShow: 4,
         slidesToScroll: 1,
         infinite: false,
+        responsive: [
+            {
+                breakpoint: 1060,
+                settings: {
+                    arrows: false,
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 768,
+                settings:{
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings:{
+                    slidesToShow: 1 
+                }
+            }
+        ]
+    })
+    $('.menu__btn, a ').on('click', function () {
+        $('.header-top').toggleClass('header-top--active')
     })
 });
